7636dd2183a8283dcc4924c225c66e8ee688c3c0
SaifSushi <alushisaif@gmail.com>
2022-03-14 15:36:14 -0500

    setting up auth

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,10 +1,87 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FormGroup, Input, Label, FormFeedback, Form } from "reactstrap";
 
-const Login = () => {
-    return(
-        <>
-        
-        </>
-    )
-}
+const Login = ({ loginRequest, storeId, isLoginInfoIncorrect, userId, userAuthToken }) => {
+  const [inputEmailValue, setinputEmailValue] = useState("");
+  const [inputPasswordValue, setInputPasswordValue] = useState("");
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginRequest(inputEmailValue, inputPasswordValue);
+    storeId(inputEmailValue);
+  };
+
+  return isLoginInfoIncorrect === false ? (
+    <Form onSubmit={(e) => handleSubmit(e)}>
+      <h3>Login</h3>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          type="email"
+          value={inputEmailValue}
+          className="login-parameters"
+          placeholder="Enter email"
+          onChange={(e) => setinputEmailValue(e.target.value)}
+        />
+        <FormFeedback valid></FormFeedback>
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          type="password"
+          value={inputPasswordValue}
+          className="login-parameters"
+          placeholder="Enter password"
+          onChange={(e) => setInputPasswordValue(e.target.value)}
+        />
+        <FormFeedback valid></FormFeedback>
+      </FormGroup>
+      <input type="submit" value={`submit`} className="login-button" />
+      <p className="create-new-account">
+        <Link to="/signup">Create a new account</Link>
+      </p>
+      <p className="forgot-password">
+        <Link to="/reset-password">Forgot password?</Link>
+      </p>
+    </Form>
+  ) : (
+    <Form onSubmit={(e) => handleSubmit(e)}>
+      <h3>Login</h3>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          type="email"
+          value={inputEmailValue}
+          className="login-parameters"
+          placeholder="Enter email"
+          onChange={(e) => setinputEmailValue(e.target.value)}
+          invalid
+        />
+        <FormFeedback valid></FormFeedback>
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          type="password"
+          value={inputPasswordValue}
+          className="login-parameters"
+          placeholder="Enter password"
+          onChange={(e) => setInputPasswordValue(e.target.value)}
+          invalid
+        />
+        <FormFeedback invalid>User Email or Password Is Incorrect</FormFeedback>
+      </FormGroup>
+      <input type="submit" value={`submit`} className="login-button" />
+      <p className="create-new-account">
+        <Link to="/signup">Create a new account</Link>
+      </p>
+      <p className="forgot-password">
+        <Link to="/resetPassword">Forgot password?</Link>
+      </p>
+    </Form>
+  );
+  // </>
+};
+
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,76 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
 import axios from 'axios'
 
-export default function Home() {
-  return (
-    <>
-    
-    </>
-      )
-}
+export default function Home({ token }) {
+	const [posts, setposts] = useState(null)
+	const [userAuthToken, setUserAuthToken] = useState('')
+	const [userId, setUserId] = useState('')
+	const [loggedIn, setLoggedIn] = useState(false)
+	const [isLoginInfoIncorrect, setIsLoginInfoIncorrect] = useState(false)
+	const headers = {
+		'Content-Type': 'application/json',
+		'Authorization': `Bearer ${userAuthToken}`,
+	}
 
-export const getServerSideProps = async (context) => {
-  try{
+	// const loginRequest = (username, password) => {
+		const loginUrl ='http://localhost:8000/api/token/'
+        useEffect(() => { 
 
-  } catch(err){
-    console.log(err)
-  }
+            axios.post(
+                loginUrl,
+				{ username: "alushi", password: "1234" },
+				{ headers: headers }
+                )
+                .then((res) => {
+                    setUserAuthToken(res.data.access)
+                    localStorage.setItem('JWT', res.data.access)
+                    setIsLoginInfoIncorrect(false)
+                    console.log(res)
+                })
+                .catch((err) => setIsLoginInfoIncorrect(true))
+            }, [])
+	// }
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/tider/post/", {headers: headers})
+        .then(res => console.log(res))
+        .catch(err => console.log("err:", err))
+
+    }, [userAuthToken])
+
+	const storeId = async (email) => {
+		const storeIdUrl = `https://protected-hollows-70202.herokuapp.com/grouper/users/${email}`
+		await axios
+			.get(
+				storeIdUrl,
+				{
+					params: {
+						email: email,
+					},
+				},
+				{ headers: headers }
+			)
+			.then((res) => {
+				setUserId(res.data)
+				localStorage.setItem('UUID', res.data._id)
+			})
+			.catch((err) => console.log(err))
+	}
+
+	return (
+		<>
+			<Head>
+				<title>Tider</title>
+				<meta name='keywords' content='reddit clone, social media, reddit' />
+			</Head>
+            <form>
+                <p>hsadas</p>
+
+            </form>
+		</>
+	)
 }
